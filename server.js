18b08731485ba34e2a51b46349836816e463da99
damian1997 <damianveltkamp@Damians-MacBook-Pro.local>
2020-04-06 13:04:41 +0200

    Socket implementation

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 4000,
       COMPONENTPATH = `${__dirname}/src/components`,
       BUNDLE = getBundleUrls()
 
+let http = require('http').createServer(app)
+let io = require('socket.io')(http)
+
 app
   .use(compression())
   .use(express.static('static'))
@@ -18,9 +21,14 @@ app
 // ROUTES
 import home from './routes/home.js'
 
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+
 app
   .get('/', (req,res) => home(req,res,COMPONENTPATH,BUNDLE))
-  .listen(PORT, () => console.log(`Using port: ${PORT}`))
+  
+http.listen(PORT, () => console.log(`Using port: ${PORT}`))
 
 function getBundleUrls() {
   const BUNDLEFILENAMES = JSON.parse(fs.readFileSync(`static/bundle/manifest.json`))
